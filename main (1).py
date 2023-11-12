67fb61b5b20744e89ba0025dae08b3f5978a8a7a
Sercan Murad <110610524+sercanmurad@users.noreply.github.com>
2023-11-12 22:04:33 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+import math
+x=int(input("Please enter x:"))
+k=int(input("Please enter k:"))
+
+polynomial=input("Please enter the polynomial:")
+p=eval(polynomial)
+if k==p:
+  print("True")
+else:
+  print("False")
